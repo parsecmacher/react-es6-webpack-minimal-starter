0348819a3421e4aabf8f16c355d94ac2d6733a4e
Peter Munro <peter@petermunro.org>
2016-03-22 13:17:24 -0700

    Updates app.jsx to use parentheses around components

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,7 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-ReactDOM.render(
-<h1>Hello, world!</h1>,
+ReactDOM.render((
+	  <h1>Hello, world!</h1>
+  ),
   document.getElementById('example')
 );
